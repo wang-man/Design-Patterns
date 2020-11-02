c5f9a25b03d373d4b65eefb8336ad4929905fdde
man.wang <man.wang@symbio.com>
2020-11-02 21:08:59 +0800

    装饰器模式及代理模式

--- Design-Patterns-test/src/patterns/decorator-ES7 装饰器模式.js
+++ Design-Patterns-test/src/patterns/decorator-ES7 装饰器模式.js
@@ -1,5 +1,7 @@
-@testDec
+// 这个例子要使用在webpack环境中，并且安装支持ES7的decorators语法插件
 
+// 第一个使用示例-------------------------------------------------------
+@testDec
 class Demo {
   constructor(){
     
@@ -11,3 +13,28 @@ function testDec(target) {
 }
 
 console.log(Demo.isDec)
+
+
+
+
+// 第二个使用示例---------------------------------------------------
+
+function mixins(list) {
+  return function (target) {    // 所有的装饰器都应该是一个函数，因为@mixins(Foo)执行了，所以需要在返回一个函数
+    Object.assign(target.prototype, list)
+  }
+}
+
+const Foo = {
+  foo(){
+    console.log('这是扩展方法')
+  }
+}
+
+@mixins(Foo)
+class Myclass {
+
+}
+
+const obj = new Myclass();
+obj.foo()
